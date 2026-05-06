99dab94b6153a6dd0829e409a207f97958de73eb
Sriharan-S <sriharan2544@gmail.com>
2026-05-06 11:59:08 +0530

    Fixes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
+import path from "node:path";
 
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
+  },
+  turbopack: {
+    root: path.resolve(),
+  },
+  experimental: {
+    preloadEntriesOnStart: false,
+    turbopackSourceMaps: false,
   },
 };
 
